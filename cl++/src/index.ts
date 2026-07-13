6d73b5e121bb7d29b7d2a442d76a9196474556c4
TraumDE <eugene301workmaik@gmail.com>
2026-07-13 03:58:47 +0300

    feat(index): add source code reading

--- cl++/src/index.ts
+++ cl++/src/index.ts
@@ -1,3 +1,26 @@
-const main = () => {};
+import fs from "fs";
+
+const main = () => {
+  const filePath = process.argv[2];
+
+  if (!filePath) {
+    console.error("Error: path to .clx file not specified");
+    process.exit(1);
+  }
+
+  if (!fs.existsSync(filePath)) {
+    console.error(`Error: not found file: ${filePath}`);
+    process.exit(1);
+  }
+
+  try {
+    const sourceCode = fs.readFileSync(filePath, "utf-8");
+
+    console.log(sourceCode);
+  } catch (error: unknown) {
+    console.error(`Error: fail to read file: ${filePath}`, error);
+    process.exit(1);
+  }
+};
 
 main();
